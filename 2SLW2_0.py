b11f5aae2946c886d032aa51ba70cb26e8ece2ca
FoxVH <162301052+FoxVH@users.noreply.github.com>
2024-05-14 12:30:14 +0300

    Задание 5 вариант
    В ЛМС смотреть 4-5 лабораторные задания.

---
+++ 2SLW2_0.py
@@ -0,0 +1,14 @@
+# Найти сумму всех n-значных чисел (1 < = n < =4). 
+while True:
+    x = input('Введите число от 1 до 4.')
+    if x == 1:
+        pass
+    elif x == 2:
+        pass
+    elif x == 3:
+        pass
+    elif x == 4:
+        pass
+    else:
+        print('Что?')
+    print(x)
